280c9cd6a93f8bcb90d503cab5563d65b59f79d5
seashell <kuanwei.dev@gmail.com>
2025-12-10 20:01:27 +0800

    spring boot servelet ver

--- src/main/java/com/seashell/jspDemo/DashboardServlet.java
+++ src/main/java/com/seashell/jspDemo/DashboardServlet.java
@@ -1,17 +1,19 @@
 package com.seashell.jspDemo;
-/*
+
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
- */
+ /*
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+*/
 
+@WebServlet(/aaa)
 public class DashboardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
---
+++ src/main/java/com/seashell/jspDemo/JspDemoApplication.java
@@ -0,0 +1,14 @@
+package com.seashell.jspDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan  // 掃描 @WebServlet 註解
+public class JspDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspDemoApplication.class, args);
+    }
+}
