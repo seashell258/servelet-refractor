36815d8e80de292075f7d07a43207869147169f3
seashell <kuanwei.dev@gmail.com>
2025-12-10 19:29:11 +0800

    tomcat ver

---
+++ src/main/java/com/seashell/jspDemo/DashboardServlet.java
@@ -0,0 +1,22 @@
+package com.seashell.jspDemo;
+/*
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+ */
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DashboardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/plain; charset=UTF-8");
+        resp.getWriter().write("Hello from servlet");
+    }
+}
